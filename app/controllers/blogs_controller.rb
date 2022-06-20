3c7b8987c5118d52202970811550d372f77ed4fe
yocajii <yocajii@stone.sakura.ne.jp>
2022-06-20 20:24:39 +0900

    blog_paramsをDRYにした

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -51,10 +51,8 @@ class BlogsController < ApplicationController
   end
 
   def blog_params
-    if current_user.premium
-      params.require(:blog).permit(:title, :content, :secret, :random_eyecatch)
-    else
-      params.require(:blog).permit(:title, :content, :secret)
-    end
+    parameters = %i[title content secret]
+    parameters << :random_eyecatch if current_user.premium
+    params.require(:blog).permit(parameters)
   end
 end
